aaad9cf6a7393b119e35fca42f50d78492a08dbc
lilmen <lilmen1313@gmail.com>
2023-02-25 16:59:22 +0300

    Добавил решение задачи 1

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,3 +1,25 @@
 ﻿// Написать программу, которая будет выдавать название дня недели по заданному номеру.
 
 Console.WriteLine("Hello, World!");
+// double a = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите число b = ");
+// double b = Convert.ToDouble(Console.ReadLine());
+// Console.ForegroundColor = ConsoleColor.Green;
+// Console.WriteLine($"Сумма a и b равна {a+b}");
+// Console.WriteLine($"Произведение a и b равно {a*b}");
+// Console.WriteLine($"Частное a и b равно {a/b}");
+// Console.WriteLine($"Сумма a и b, возведённая в квадрат {(a+b)*(a+b)}");
+// Console.WriteLine($"Разность a и b равна {a - b}");
+// Console.WriteLine($"Hi!");
+// Console.WriteLine($"Hi Илья!");
+// Console.ForegroundColor = ConsoleColor.Blue;
+// Console.WriteLine("Hi " + "Ilya");
+// Console.WriteLine($"а = {a:F2} b = {b:F2}");
+// Console.WriteLine($"FF");
+// Console.WriteLine($"FF_01");
+// Console.WriteLine($"CP");
+// Console.WriteLine($"CP2");
+// Console.WriteLine($"CP3");
+// Console.ForegroundColor = ConsoleColor.White;
+// Console.WriteLine($"local");
+// Console.WriteLine($"Amend");
