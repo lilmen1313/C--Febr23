badcc6e8ce8e272c3280c56570318150dfc8dc89
lilmen <lilmen1313@gmail.com>
2023-02-25 21:32:25 +0300

    Добавил решение задачи 7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -7,5 +7,18 @@
 
 Console.WriteLine("Введите целое трёхзначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-
-
+    if (-1000 < a && a < -99 )
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(-a % 10);
+    }
+    else if (a > 99 && a < 1000)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write(a % 10);
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Введено не трехзначное число!");
+    }
