88e8cc90b3a8d5666f131df52c88a5f342a40d8e
lilmen <lilmen1313@gmail.com>
2023-02-26 19:42:02 +0300

    Добавил решение задачи 6

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.WriteLine("Введите целое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write($"Число {a} чётное, т.к. кратно двум");
+    }
+else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write($"Число {a} нечётное, т.к. не кратно двум");
+    }
