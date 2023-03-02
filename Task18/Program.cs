d14c940ba8ba0d9b19144f4be244f466fd6ee459
lilmen <lilmen1313@gmail.com>
2023-03-02 20:45:47 +0300

    Добавил решение задачи 18 с помощью метода

---
+++ Task18/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Method(n));
+
+string Method (int a)
+{
+    if (a == 1) return "x > 0, y > 0";
+    if (a == 2) return "x < 0, y > 0";
+    if (a == 3) return "x < 0, y < 0";
+    if (a == 4) return "x > 0, y < 0";
+    return "Такой четверти не существует!";
+}
