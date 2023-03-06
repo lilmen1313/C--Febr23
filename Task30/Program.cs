5d9f0d1d0166161f5c6bdeb7100dc0f4b92865eb
lilmen <lilmen1313@gmail.com>
2023-03-06 22:22:52 +0300

    Добавил решение задачи 30 с помощью метода(массив)

---
+++ Task30/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.Write("Введите размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] mas = Mass(a);
+Print(mas);
+
+int[] Mass(int n)
+{
+    int[] array = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void Print(int[] array11)
+{
+    for (int i = 0; i < array11.Length; i++)
+    {
+        Console.Write(array11[i] + " ");
+    }
+    return;
+}
