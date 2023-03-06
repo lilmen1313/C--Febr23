e022bd5e8f37b6c5cacdd3ad59bfeebdcc085c21
lilmen <lilmen1313@gmail.com>
2023-03-06 20:34:43 +0300

    Добавил решение задачи 24 с помощью метода

---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int summa = Method(a);
+Console.WriteLine($"Сумма чисел от 1 до {a} равнно {summa}");
+
+int Method (int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
