f2c42ac68a472400cc88be7e5b3db637d024da74
lilmen <lilmen1313@gmail.com>
2023-03-06 21:10:37 +0300

    Добавил решение задачи 26 с помощью метода

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine($"Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int y = Method (a);
+Console.WriteLine($"Число {a} состоит из {y} цифры/-р");
+
+int Method (int n)
+{
+    int i = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        i += 1;
+    }
+    return i;
+}
