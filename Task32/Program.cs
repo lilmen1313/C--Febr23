ae80d5acc9b0b8f2eafc19a28a66bf621ffec2d9
lilmen <lilmen1313@gmail.com>
2023-03-09 20:46:33 +0300

    Добавил решение задачи 32

---
+++ Task32/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] array = CreateArrayRndInt(8, -20, 20);
+PrintArray(array);
+
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length-1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+
+    Console.Write("["); // замена знаков элементов массива
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length-1) Console.Write($"{-arr[i]}, ");
+        else Console.Write($"{-arr[i]}");
+    }
+    Console.WriteLine("]");
+}
