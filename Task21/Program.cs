9ab73e424a1cc628b9245b83cfcd5dc5f23d9aaa
lilmen <lilmen1313@gmail.com>
2023-03-09 18:21:04 +0300

    Доработал задачу 21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -3,22 +3,15 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-
 Console.WriteLine("Введите координаты точки А");
-Console.Write("Введите координаты точки X: ");
-int xA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты точки Y: ");
-int yA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты точки Z: ");
-int zA = Convert.ToInt32(Console.ReadLine());
+int xA = Input("Введите координаты точки X: ");
+int yA = Input("Введите координаты точки Y: ");
+int zA = Input("Введите координаты точки Z: ");
 
 Console.WriteLine("Введите координаты точки B");
-Console.Write("Введите координаты точки X: ");
-int xB = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты точки Y: ");
-int yB = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты точки Z: ");
-int zB = Convert.ToInt32(Console.ReadLine());
+int xB = Input("Введите координаты точки X: ");
+int yB = Input("Введите координаты точки Y: ");
+int zB = Input("Введите координаты точки Z: ");
 
 double length = Distance (xA, yA, zA, xB, yB, zB);
 Console.WriteLine($"Растояние между точками А и В равно {Math.Round(length, 2, MidpointRounding.ToZero)}");
@@ -26,4 +19,10 @@ Console.WriteLine($"Растояние между точками А и В рав
 double Distance (int x1, int y1, int z1, int x2, int y2, int z2)
 {
     return Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+}
+
+int Input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
