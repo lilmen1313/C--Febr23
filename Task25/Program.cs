d1708a229cf8eae1bdbb8d08d31392c19c667f7f
lilmen <lilmen1313@gmail.com>
2023-03-11 22:16:41 +0300

    Добавил решение задачи 25 (возведение в натуральную степень)

---
+++ Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int number1 = Input("Введите число А: ");
+int number2 = Input("Введите натуральную степень В : ");
+int mul = Exponentiation(number1, number2);
+Console.WriteLine($"Число {number1} в степени {number2} равно {mul}");
+
+
+int Exponentiation(int num1, int num2)
+{
+    int multi = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        checked {multi = num1 * multi;};
+    }
+    return multi;
+}
+
+int Input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
