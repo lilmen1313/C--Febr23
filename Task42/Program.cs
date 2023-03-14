df9971660ccd0ca600e5e2d51b2fa9cfb65f092e
lilmen <lilmen1313@gmail.com>
2023-03-14 23:08:59 +0300

    Добавил задачу 42 (преобразование десятичного числа в двоичное)

---
+++ Task42/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int a = Input("Введите число а: ");
+Console.WriteLine(Translation(a));
+
+int Translation(int num)
+{
+    int result = 0;
+    int dec = 1;
+
+    while (num != 0)
+    {
+        int remainder = num % 2;
+        result = result + remainder * dec;
+        dec = dec * 10;
+        num = num / 2;
+    }
+    return result;
+}
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
