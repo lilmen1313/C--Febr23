0feef6f57833a7679bdea8ee5180905beb52a01e
lilmen <lilmen1313@gmail.com>
2023-03-16 22:02:52 +0300

    Добавил решение задачи 46 ( заполнение матрицы случайными числами) + изменил название предыдущего коммита

---
+++ Task46/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int strok = Input("Введите кол-во строк в массиве m: ");
+int stolb = Input("Введите кол-во столбцов в массиве n: ");
+int[,] matrixNew = CreateMatrixRndInt(strok, stolb, 10, 50);
+PrintMatrix(matrixNew);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random(); // создаем экземпляр класса вне цикла
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 3}");
+            
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
