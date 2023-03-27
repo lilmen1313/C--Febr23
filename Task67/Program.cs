4be777bf336625e1fd3751950a92ca3f78ab6596
lilmen <lilmen1313@gmail.com>
2023-03-27 17:32:30 +0300

    Добавил решение задачи 67 (Рекурсия, сумма цифр числа)

---
+++ Task67/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int number = Input("Введите целое положительное число: ");
+
+int num = SumNumbersDigits(number);
+Console.Write(num);
+
+int SumNumbersDigits(int n)
+ {
+     if (n == 0) return 0;
+     else return n % 10 + SumNumbersDigits(n / 10);
+ }
+
+ int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
