4d26805f0bf55bcb7c26a0b5de09b4ba28c148ed
lilmen <lilmen1313@gmail.com>
2023-03-27 17:30:27 +0300

    Добавил решение задачи 63 (Рекурсия, вывод чисел от 1 до N)

---
+++ Task63/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// РЕКУРСИЯ - вызов функции самой себя.
+
+int number = Input("Введите целое положительное число N: ");
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return; // условие выхода
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
